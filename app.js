abc335d12809b6a2b336a0a24da27af0662f5e82
sgpedro <pedro.guemureman@hotmail.com>
2018-07-17 21:05:18 +0000

    Primeiros Commits

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("main");
+});
+
+app.get("/quemsomos", function(req, res){
+    res.render("apresentacao");
+});
+
+app.get("/produtos", function(req, res){
+    res.render("produtos");
+});
+
+app.get("/receitas", function(req, res){
+    res.render("receita");
+});
+
+app.get("/ondenosencontrar", function(req, res){
+    res.render("ondeencontrar");
+});
+
+app.get("/contato", function(req, res){
+    res.render("contato");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("server is listening"); 
+});
